a3ff4d68464c9907f622189365818e16af9958d9
Jin-Hao Young <z430035566@gmail.com>
2017-04-09 23:03:09 +0800

    123

---
+++ lab6/Program.cs
@@ -0,0 +1,46 @@
+﻿using java.io;
+using opennlp.tools.tokenize;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (StreamWriter sw = new StreamWriter("example.txt"))
+            {
+                string[] files = Directory.GetFiles(@"..\..\..\Dataset\", "*.html");
+                foreach (var file in files)
+                {
+                    using (StreamReader sr = new StreamReader(file))
+                    {
+                        while (sr.Peek() != -1)
+                        {
+                            string line = sr.ReadLine();
+                            InputStream modelIn = new FileInputStream(@"..\..\..\en-token.bin");
+                            string[] tokens;
+
+                            TokenizerModel model = new TokenizerModel(modelIn);
+                            TokenizerME Tokenizer = new TokenizerME(model);
+
+                            tokens = Tokenizer.tokenize(line);
+                            for (int i = 0;i<tokens.Length;i++)
+                            {
+                                sw.WriteLine(tokens[i]+" ");
+                            }
+
+                            
+                        }
+                        
+                    }
+                }
+            }
+        }
+    }
+}
